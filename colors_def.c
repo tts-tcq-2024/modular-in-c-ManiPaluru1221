fdfbb6f481b2c332da5e3f3685135ed8efe71ce2
ManiPaluru1221 <palurumani1999@gmail.com>
2024-08-04 13:08:53 +0530

    Create colors_def.c

---
+++ colors_def.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <assert.h>
+#include <color_pair_def.h>
+
+MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+MAX_COLORPAIR_NAME_CHARS = 16;
+numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
