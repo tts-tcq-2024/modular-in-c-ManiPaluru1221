cbd6d2d7437b0f16d08908d513053201b58c10b3
ManiPaluru1221 <palurumani1999@gmail.com>
2024-08-04 14:03:06 +0530

    Update colors_def.c

--- colors_def.c
+++ colors_def.c
@@ -1,7 +1,1 @@
-#include "color_pair_def.h"
 
-numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-
-numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
