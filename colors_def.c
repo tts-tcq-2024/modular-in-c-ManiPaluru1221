156cd88dee8c95f719da8c6538303a4fef24e5f0
ManiPaluru1221 <palurumani1999@gmail.com>
2024-08-04 13:28:19 +0530

    Update colors_def.c

--- colors_def.c
+++ colors_def.c
@@ -1,16 +1,7 @@
 #include "color_pair_def.h"
 
-MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
 numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-
-MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-MAX_COLORPAIR_NAME_CHARS = 16;
 
 numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
