2d3df90b57a606efe00f1a921870563f69541379
ManiPaluru1221 <palurumani1999@gmail.com>
2024-08-04 13:21:48 +0530

    Create color_pair_utility.c

---
+++ color_pair_utility.c
@@ -0,0 +1,7 @@
+#include "color_pair_def.h"
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
